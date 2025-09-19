35d4ccc983197f2dec4d5643e64a00e758b64677
Bharti <110337668+Bharti025@users.noreply.github.com>
2025-09-19 23:19:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+let digit=document.querySelectorAll(".code");
+digit.forEach((input,index)=>{
+	input.addEventListener('input',(e)=>{
+		const value=e.target.value;
+	if(index<digit.length-1){
+		digit[index+1].focus();
+	};
+});
+})
+
+digit.forEach((input,index)=>{
+	input.addEventListener('keydown',(e)=>{
+	if(e.key=="Backspace" && index>0){
+		e.target.value="";
+		digit[index-1].focus();
+	};
+});
+})
